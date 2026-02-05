92eb7636669cea5344ae09714aa5104c5dc9072d
popovs <sarah.popov@gmail.com>
2026-02-05 10:56:08 -0800

    Add license boilerplate

--- R/collar_processing_fxns.R
+++ R/collar_processing_fxns.R
@@ -1,10 +1,24 @@
+
+# Copyright 2026 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ## PROCESS COLLAR KEYS
 
 # Functions to pull collar data off Vectronix website, using
 # the supplied collar keys, then process and clean data.
 
 # AUTHORSHIP WHERE NOTED: Angus Smith @angus-smith
-
 
 ## collar data cleaning
 
--- R/hr_overlap_fxns.R
+++ R/hr_overlap_fxns.R
@@ -1,5 +1,19 @@
-# Percent overlap functions
+# Copyright 2026 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+
+# Percent overlap functions
 
 prct_overlap <- function(shp_1, shp_2, 
                          join_cols = c("animal_id", "year"),
